3256b1b8c7f77a019f8bd0e1763608f26ae2055b
Joey Domino <playdude224@hotmail.com>
2018-07-03 19:01:39 -0400

    Call to Cleverbot done
    Cleverbot called and data returned correctly. Need to parse through and print response.

--- marvin.py
+++ marvin.py
@@ -1,7 +1,8 @@
 import time
 from slackclient import SlackClient
+import cleverbot
+import requests
 
-# TO DO: CHECK FOR USER ID. DO NOT DO ANYTHING IF ID IS MARVIN
 #---------------------------#
 #Programmer: Joey Domino    #
 #Date: 7/2/18               #
@@ -10,6 +11,9 @@ from slackclient import SlackClient
 #---------------------------#
 
 SLACK_TOKEN = 'xoxb-8483522179-392696063798-IItvotS2Kd5pMiszkQarYeMx'
+CLEVERBOT_TOKEN = 'CC8lzaIMoM8kVAcwNw36CJHfGDg'
+#Must append URL with ?input=input&key=CLEVERBOT_TOKEN to function
+CLEVERBOT_URL = 'https://www.cleverbot.com/getreply'
 marvin_id = None
 READ_WEB_DELAY = 1   #1 second delay between each RTM read
 
@@ -20,6 +24,10 @@ READ_WEB_DELAY = 1   #1 second delay between each RTM read
 #Returns: response (API's response to user message)
 #------------------------------------------------------#
 def call_clever_bot(message):
+    url_payload = {'input' : message, 'key' : CLEVERBOT_TOKEN}
+    return_data = requests.get(CLEVERBOT_URL, url_payload)
+    #data received correctly. Get response to print now.
+    
     return
 
 #------------------------------------------------------#
@@ -30,13 +38,22 @@ def call_clever_bot(message):
 #------------------------------------------------------#
 def parse_command(slack_events):
     for event in slack_events:
-        if "user" in event:
-            if event["user"] != "UBJLG1VPG":
-                if event["type"] == "message" and not "subtype" in event:
-                    if "!m" in event["text"].lower() or "marvin" in event["text"].lower():
-                        print(event["channel"])
-                        return event["text"], event["channel"]
-    print("marvin not called")  
+        print(event)
+        #Check event type, then check if user is in it.
+        if event["type"] == "message":
+            if "user" in event:
+                if event["user"] != "UBJLG1VPG":
+                    event_text = event["text"].lower()
+                    if event["type"] == "message" and not "subtype" in event:
+                        if event_text.startswith("!m") or "marvin" in event_text:
+                            if event_text.startswith("!m"):
+                                event_text = event_text[3:]
+                            return event_text, event["channel"]
+        #Idea: have bot respond snarkily if used is in "typing" state for a minute.
+        #if event["type"] == "typing":
+
+        #Idea: make another check for if marvin is @'ed
+        #if event["type"] == 'desktop_notification':
     return None, None
 
 #------------------------------------------------------#
@@ -46,11 +63,15 @@ def parse_command(slack_events):
 #Returns: none
 #------------------------------------------------------#
 def handle_command(command, channel):
-    slack_client.api_call(
-        "chat.postMessage",
-        channel = channel,
-        text = command,
-        as_user = True)
+    #if 
+    print(command)
+    call_clever_bot(command)
+    #print(clever_bot_response)
+    #slack_client.api_call(
+    #    "chat.postMessage",
+    #    channel = channel,
+    #    text = command,
+    #    as_user = True)
 
 #main gatta be on the bottom. Code runs top to bottom. Functions need to be init'd first. For future notice (because Joey forgets things).
 #------------------------------------------------------#
