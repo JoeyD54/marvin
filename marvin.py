9c918489d635899bddf55cbf8b34e641a93a3476
Joey Domino <playdude224@hotmail.com>
2018-07-06 17:44:37 -0400

    YelpAPI Integration
    Started integration of Yelp API. Currently returning correct data. Need to parse through mutli trees to get business name and rating.

--- marvin.py
+++ marvin.py
@@ -2,6 +2,7 @@ import time
 from slackclient import SlackClient
 import cleverbot
 import requests
+from yelpapi import YelpAPI
 
 #---------------------------#
 #Programmer: Joey Domino    #
@@ -12,6 +13,7 @@ import requests
 
 SLACK_TOKEN = 'xoxb-8483522179-392696063798-IItvotS2Kd5pMiszkQarYeMx'
 CLEVERBOT_TOKEN = 'CC8lzaIMoM8kVAcwNw36CJHfGDg'
+YELP_TOKEN = 'nxnPpM0ToXtVZw7FholFve7bZQSLfxf_N3Zpq6zlJxAiBcaBugGNz-ddYGHCxzVnIQmfVFqMWum2POFmJDNXpQ2o9cWECukgLWkOhh7_uMujUA3cZBCtqHNvqck_W3Yx'
 #Must append URL with ?input=input&key=CLEVERBOT_TOKEN to function
 CLEVERBOT_URL = 'https://www.cleverbot.com/getreply'
 marvin_id = None
@@ -26,6 +28,7 @@ READ_WEB_DELAY = 1   #1 second delay between each RTM read
 #Returns: command, channel
 #------------------------------------------------------#
 def parse_command(slack_events):
+    clever_bot_called = 0
     for event in slack_events:
         if "channel" in event:
             if event["channel"] == "C08E7RN72":
@@ -42,22 +45,49 @@ def parse_command(slack_events):
                                     if event_text.startswith("!m"):
                                         event_text = event_text[3:]
                                     return event_text, event["channel"]
+
             #Idea: have bot respond snarkily if used is in "typing" state for a minute.
             #if event["type"] == "typing":
 
-            #Idea: make another check for if marvin is @'ed
+                #Another check for if marvin is @'ed
                 if event["type"] == 'desktop_notification':
                     if "content" in event:
-                        if "leave channel" in event["content"]:
-                            channel = event["channel"]
+                        #Yelp food implementation
+                        if "@marvin" and "lunch" in event["content"]:
                             slack_client.api_call(
                                 "chat.postMessage",
-                                channel = channel,
-                                text = "Leaving channel: <!channel>",
+                                channel = event["channel"],
+                                text = "What're ya cravin?",
                                 as_user = True)
-                            slack_client.api_call(
-                                "channels.leave",
-                                channel = channel)
+                            lunch_check = True
+                            while lunch_check:
+                                for lunch_event in slack_client.rtm_read():
+                                    if "text" in lunch_event:
+                                        if lunch_event["text"] == "nevermind":
+                                            slack_client.api_call(
+                                                "chat.postMessage",
+                                                channel = event["channel"],
+                                                text = "Alright.",
+                                                as_user = True)
+                                            lunch_check = False
+                                        #currently returns 3 headers "businesses, total, and region. Need to dip into businesses to get name and ratings.
+                                        if lunch_event["text"] == "food":
+                                            yelp_api = YelpAPI(YELP_TOKEN)
+                                            print("\n\n")
+                                            search_results = yelp_api.search_query(term = 'food', location = 'Detroit, MI', sort_by = 'rating', limit = 5)
+                                            for header in search_results:
+                                                for topic in header:
+                                                    print(topic)
+                                                    #slack_client.api_call(
+                                                    #    "chat.postMessage",
+                                                    #    channel = event["channel"],
+                                                    #    text = topic + "\n\n",
+                                                    #    as_user = True)
+                                            #print(search_results + "\n\n")
+                                            lunch_check = False
+                                    
+                            
+                            
     return None, None
 
 #------------------------------------------------------#
@@ -66,9 +96,9 @@ def parse_command(slack_events):
 #Arguments: command, channel
 #Returns: none
 #------------------------------------------------------#
-def handle_command(command, channel, cb_convo):
-    #if 
-    #print(command)
+def handle_command(command, channel, cb_convo): 
+    print(command)
+    
     response = call_clever_bot(command, cb_convo)
     #print(response)
     slack_client.api_call(
@@ -86,12 +116,8 @@ def handle_command(command, channel, cb_convo):
 def call_clever_bot(message, clever_bot_convo):
     #url_params = {'input' : message, 'key' : CLEVERBOT_TOKEN}
     try:
-        #cb = cleverbot.Cleverbot(CLEVERBOT_TOKEN, cs='76nxdxIJ02AAA', timeout=60, tweak1=0, tweak2=100, tweak3=75)
-        #convo = cb.conversation()
         reply = clever_bot_convo.say(message)
         return reply
-    #data received correctly. Get response to print now.
-        #return reply.get("output", None)
     except cleverbot.APIError as e:
         print(e.error, e.status)
 
